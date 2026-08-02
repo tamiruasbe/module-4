a880618e752a25b32e4cb5c406e3b4950dbf0686
Tamiru Asbe <tamiruasbe10@gmail.com>
2026-08-02 21:52:53 +0300

    module-7/session-2

--- TmsApi.Api/Controllers/EnrollmentsController.cs
+++ TmsApi.Api/Controllers/EnrollmentsController.cs
@@ -11,43 +11,17 @@ namespace TmsApi.Api.Controllers;
 [ApiVersion("2.0")]
 public class EnrollmentsController(IMediator mediator) : ControllerBase
 {
-     [HttpGet]
-    public IActionResult GetAll()
-    {
-        var enrollments = new List<object>
-        {
-            new
-            {
-                id = "1",
-                studentId = 1001,
-                studentName = "Liya Kebede",
-                courseId = 1,
-                courseName = "Advanced Java Services",
-                status = "Pending",
-                enrolledAt = DateTime.UtcNow
-            },
-            new
-            {
-                id = "2",
-                studentId = 1002,
-                studentName = "Abel Bekele",
-                courseId = 2,
-                courseName = "Cloud Computing",
-                status = "Approved",
-                enrolledAt = DateTime.UtcNow
-            }
-        };
 
-        return Ok(enrollments);
-    }
+[HttpGet]
+public async Task<IActionResult> GetAll(
+    CancellationToken ct)
+{
+    var result = await mediator.Send(
+        new GetEnrollmentsQuery(),
+        ct);
 
-    [HttpPost("{id}/approve")]
-    public IActionResult Approve(string id)
-    {
-        Console.WriteLine($"Enrollment {id} approved.");
-        return NoContent();
-    }
-
+    return Ok(result);
+}
     [HttpPost]
     public async Task<IActionResult> Enroll(
         EnrollStudentCommand command,
