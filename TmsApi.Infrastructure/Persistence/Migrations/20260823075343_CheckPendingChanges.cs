68696ff3e3562fb4e851bff4850645a23a3f78d6
Tamiru Asbe <tamiruasbe10@gmail.com>
2026-08-23 10:59:56 +0300

    module-11/session-2

---
+++ TmsApi.Infrastructure/Persistence/Migrations/20260823075343_CheckPendingChanges.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TmsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CheckPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstructorId",
+                table: "Courses",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstructorId",
+                table: "Courses");
+        }
+    }
+}
